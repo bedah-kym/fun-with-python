947722ae6fe2fd47a47d4a2b15db82b398dd4bc9
BEDAH <>
2022-08-18 08:22:47 +0300

    changed staticmethod to age counter

--- class methods.py
+++ class methods.py
@@ -4,11 +4,12 @@ class Employee:
         self.lname=lname
  
     @staticmethod
-    def validate(num1,num2):
-        if (num1 + num2 ) < 0:
+    def validate(current_year,birth_year):
+        age=current_year - birth_year
+        if age < 18:
             return ("brooo fuck off")
         else:
-            return num1+num2
+            print(f'you are {age} years which is legal')
 
     def combine(self):
         print (self.fname,self.lname)
@@ -22,11 +23,9 @@ class Employee:
 
 print(Employee.fullname('beda').combine())
 e2 = Employee('kim','yo')
-#print(Employee.validate(7,2))
+print(Employee.validate(2022,1999))
 print(e2.combine())
-#print(e2.combine)
 
-#print(e.validate())
     
 #hi furture me i left this code with  a bug coz i know ur dumb ass already forgot this shit
 # fix it without sololearn or google 
