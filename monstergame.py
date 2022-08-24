dd25a8b3a367c5cf1e68f5655b2cbffb618cac5d
BEDAH <>
2022-08-24 09:56:54 +0300

    fixed the wins and level bugs now game works fine

--- monstergame.py
+++ monstergame.py
@@ -1,5 +1,9 @@
-from random import *
+""" in this game we have two types of monsters , aliens and preditors now each can get a match to play aginist
+each other or the other party .each match has a shooter and a victim the shooter kills the victim and gets
+renewed life for the next match but since the shooter has less bullets it gets killed in the second match
+and the victim in the second match becomes the shooter in the next match and dies there and so on """
 
+# i use this game to learn python oop and edge cases in programs
 
 class Monster:
     def __init__(self,name,origin):
@@ -16,13 +20,16 @@ class Monster:
                 other.life-=1
                 self.bullets-=1
             else:
+                 other.wins+=1
+                 other.life = 3
                  print('you have no bullets')
                  
         if other.life < 1 :
             self.wins+=1
             self.life = 3
-        print(self.wins,'wins')
-            
+
+
+        print('-.>>>>>>>>>round----')     
     
     def add_life(self):
         self.life+=1
@@ -35,7 +42,7 @@ class Monster:
                 self.bullets+=5
         return self.level
     
-
+     
 
 class Alien(Monster):
     def __init__(self,name,origin,weapon):
@@ -69,11 +76,11 @@ class Preditor(Monster):
     
 A1=Alien('bert','mars','ak47')
 M1=Preditor('ugly','jupiter','steel plate')
-M2=Preditor('ugly','jupiter','steel plate')
-M3=Preditor('ugly','jupiter','steel plate')
+M2=Preditor('mose','jupi','steel')
+M3=Preditor('ujing','jupit','steel-plate')
 
 
-print('-------------- match----------')
+print('-------------- match 1 ----------')
 
 
 while M1.life>0 :
@@ -83,21 +90,27 @@ while M1.life>0 :
     print('shoter has',A1.wins,'wins and','level',A1.rank(),'with',A1.bullets,'bullets',A1.life,'life')
     print('victim has',M1.wins,'wins and','level',M1.rank(),'with',M1.bullets,'bullets',M1.life,'life')
 
-print('-------------- match----------')
+print('-------------- match 2 ----------')
     
-while M2.life>0 :
-    A1.shoot(M1)
-    M2.shootback(A1)
-    #print(M1.details())
-    print('shoter has',A1.wins,'wins and','level',A1.rank(),'with',A1.bullets,'bullets',A1.life,'life')
-    print('victim has',M2.wins,'wins and','level',M2.rank(),'with',M2.bullets,'bullets',M2.life,'life')
+while M2.life>0 and M2.bullets>0:
+    if A1.life >0 or A1.bullets >0 :
+        A1.shoot(M2)
+        M2.shootback(A1)
+        print('shoter has',A1.wins,'wins and','level',A1.rank(),'with',A1.bullets,'bullets',A1.life,'life')
+        print(M2.name,'has',M2.wins,'wins and','level',M2.rank(),'with',M2.bullets,'bullets',M2.life,'life')
+    else:
+        print('shooter is dead')
+        break
+print('-------------- match 3 ----------')
     
-print('-------------- match----------')
-    
-while M3.life>0 :
-    A1.shoot(M1)
-    M2.shootback(A1)
-    #print(M1.details())
-    print('shoter has',A1.wins,'wins and','level',A1.rank(),'with',A1.bullets,'bullets',A1.life,'life')
-    print('victim has',M3.wins,'wins and','level',M3.rank(),'with',M3.bullets,'bullets',M3.life,'life')
-    
+while M3.life>0 and M3.bullets>0:
+    if M2.life >0 or M2.bullets >0 :
+        M2.shoot(M3)
+        M3.shootback(M2)
+        print('shoter has',M2.wins,'wins and','level',M2.rank(),'with',M2.bullets,'bullets',M2.life,'life')
+        print(M3.name,'has',M3.wins,'wins and','level',M3.rank(),'with',M3.bullets,'bullets',M3.life,'life')
+    else:
+        print('shooter is dead')
+        break
+
+
